ca50fabb3fbcf872e683d7deb99fd188330f61e2
Thierry Chatel <tchatel@gmail.com>
2013-10-21 18:09:08 +0200

    avant meetup

--- js/controllers.js
+++ js/controllers.js
@@ -4,25 +4,29 @@
 
 angular.module('gamestore.controllers', ['gamestore.services'])
 
-.controller('MainCtrl', ['$scope', '$location', 'cart', function ($scope, $location, cart) {
+.controller('MainCtrl', ['$scope', '$location', 'cart',
+                         function ($scope, $location, cart) {
     $scope.addCart = function (game) {
         cart.add(game);
         $location.url("/cart");
     }
 }])
 
-.controller('CatalogCtrl', ['$scope', 'catalogPromise', function ($scope, catalogPromise) {
+.controller('CatalogCtrl', ['$scope', 'catalogPromise',
+                            function ($scope, catalogPromise) {
     catalogPromise.getList().then(function (data) {
         $scope.catalog = data;
     });
 }])
 
-.controller('GameCtrl', ['$scope', '$routeParams', 'catalogPromise', function ($scope, $routeParams, catalogPromise) {
+.controller('GameCtrl', ['$scope', '$routeParams', 'catalogPromise',
+                         function ($scope, $routeParams, catalogPromise) {
     catalogPromise.getItem($routeParams.ref).then(function (data) {
         $scope.game = data;
     });
 }])
 
-.controller('CartCtrl', ['$scope', 'cart', function ($scope, cart) {
+.controller('CartCtrl', ['$scope', 'cart',
+                         function ($scope, cart) {
     $scope.cart = cart;
 }])
